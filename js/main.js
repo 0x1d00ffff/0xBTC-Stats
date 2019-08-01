42c3ba272703c9f3728591a519616f71327a2ba3
0x1d00ffff <37033742+0x1d00ffff@users.noreply.github.com>
2019-07-31 21:37:38 -0700

    update to infura v3 api url

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ const _MINIMUM_TARGET_BN = new Eth.BN(_MINIMUM_TARGET);
 const _IDEAL_BLOCK_TIME_SECONDS = _ETH_BLOCKS_PER_REWARD * _SECONDS_PER_ETH_BLOCK;
 
 /* TODO: figure out why it doesn't work w metamask */
-var eth = new Eth(new Eth.HttpProvider("https://mainnet.infura.io/MnFOXCPE2oOhWpOCyEBT"));
+var eth = new Eth(new Eth.HttpProvider("https://mainnet.infura.io/v3/bffcddb94d02462db783eb08d7f97f1f"));
 // if (typeof window.web3 !== 'undefined' && typeof window.web3.currentProvider !== 'undefined') {
 //   var eth = new Eth(window.web3.currentProvider);
 // } else {
